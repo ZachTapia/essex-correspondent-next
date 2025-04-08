38318d8ca41a8497f8119289c0b2bf40da0b9902
Bradyn Wolkoys <bnwolkoys@gmail.com>
2025-04-08 12:09:28 -0700

    started on about page

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+import Footer from "../shared/Footer";
+import NavBar from "../shared/NavBar";
+import SectionHeader from "./components/SectionHeader";
+
+
+const AboutPage = () => {
+    return (
+        <>
+        <main className="h-full w-full bg-white">
+            <NavBar />
+            <SectionHeader />
+
+            <Footer />
+        
+
+        </main>
+        </>
+    )
+};
+
+export default AboutPage;

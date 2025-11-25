2622b8000ff9e1857fe0d569f29e2be95ba2a18a
Bradyn Wolkoys <bnwolkoys@gmail.com>
2025-11-25 08:50:28 -0800

    updated webinar link

--- app/resources/components/AccessTraining.tsx
+++ app/resources/components/AccessTraining.tsx
@@ -6,7 +6,7 @@ const AccessTraining = () => {
     <section className="w-full h-full justify-items-center bg-white mx-auto max-w-screen-lg p-4 max-sm:pt-0">
       <div className="m-2 p-2 gap-4">
         <a
-          href="https://us02web.zoom.us/rec/play/CfyFeea4bqfTKPPNQd4e2jXsGv_WsuadhsH3gABn0eAoNijTEamq93tccqHB8DqwGlLAr1AR5bI3WfJ6.Fh4udzAuYc4Hj0iN?canPlayFromShare=true&from=share_recording_detail&continueMode=true&componentName=rec-play&originRequestUrl=https%3A%2F%2Fus02web.zoom.us%2Frec%2Fshare%2FI1TkHhnCZhoO_PEEGoti-Nd9u1bkx00dJTNDxpvzszUvVAob-dt_aP5qWeJ-g9B0.z72-Lb8DdbvCUKwI"
+          href="https://us02web.zoom.us/rec/play/J66TTLCmHK99EXGjt7GlKlWnhaXsAnaHyXdWVzxTwbRnWB8HJuSER9-nuagsaTw93nDUe6g_ucGMxWH1.mIh6NiGOIqQ4BJ24?eagerLoadZvaPages=sidemenu.billing.plan_management&accessLevel=meeting&canPlayFromShare=true&from=share_recording_detail&continueMode=true&componentName=rec-play&originRequestUrl=https%3A%2F%2Fus02web.zoom.us%2Frec%2Fshare%2FMm_rh6uR_JEb55qFmW1ZdBpIi8aWXIvZjV-EZh1HPIpqpzdg_EUokaATqBqZEKQK.gs7pknmARh9Bk9UM"
           target="_blank"
           rel="noopener noreferrer"
           className="cursor-pointer"

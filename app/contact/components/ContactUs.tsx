148bd739048892e6e331b3e2d394000c8b97de35
Bradyn Wolkoys <bnwolkoys@gmail.com>
2025-12-22 10:10:13 -0800

    updated syntax

--- app/contact/components/ContactUs.tsx
+++ app/contact/components/ContactUs.tsx
@@ -2,13 +2,13 @@ export default function ContactUs() {
 
   return (
     <>
-        <section className="w-full h-full bg-gray-200 mx-auto max-w-screen-lg flex flex-col p-4 pt-10 max-sm:pt-10">
+        <section className="w-full h-full bg-gray-200 mx-auto max-w-5xl flex flex-col p-4 pt-10 max-sm:pt-10">
        
         <div className="w-full bg-gray-200 rounded-lg overflow-hidden shadow">
           <iframe
             src="https://airtable.com/embed/appaCkjJJOhvI0Jh9/pagKjI9DSXhtY3359/form"
             title="Airtable Contact Form"
-            className="w-full h-[533px]"
+            className="w-full h-133.25"
             style={{
               background: "transparent",
               border: "1px solid #ccc",

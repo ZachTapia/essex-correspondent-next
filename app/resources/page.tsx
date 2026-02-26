5e0b72dc35a13b95f1a68feea2aca5a48ce19118
Bradyn Wolkoys <bnwolkoys@gmail.com>
2026-02-26 13:30:09 -0800

    removed webinar waiting for new one

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -18,7 +18,7 @@ const ResourcePage = () => {
             <ThingsToKnow />
             <MarketingMaterials />
             <ResourceDocs />
-            <AccessTraining />
+            {/* <AccessTraining /> */}
             <EssexDifference />
       
 

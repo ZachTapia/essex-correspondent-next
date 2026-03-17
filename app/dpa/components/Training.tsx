a17ea01551881b54efb26de67e58152ca6a4dd9d
Bradyn Wolkoys <bnwolkoys@gmail.com>
2026-03-17 09:27:02 -0700

    updated video to google for dpa

--- app/dpa/components/Training.tsx
+++ app/dpa/components/Training.tsx
@@ -1,29 +1,31 @@
+import Image from "next/image";
+import image1 from "../../../public/dpa/webinarVideo.svg";
+
 const Training = () => {
   return (
     <>
-      <section className="w-full h-full bg-white mx-auto max-w-5xl flex flex-col p-4 pt-10 max-sm:pt-10">
-        <h1 className="text-blue-950 text-center text-4xl font-light ">
-          Training
-        </h1>
-        <hr className="w-40 h-1.5 mx-auto bg-gray-100 border-0 my-4 dark:bg-blue-950"></hr>
-      </section>
-
       <section className="w-full h-full justify-items-center bg-white mx-auto max-w-5xl p-4 max-sm:pt-0">
-  <div className="m-2 p-2 gap-4 w-full flex justify-center">
-    <iframe
-      src="https://essexmortgage1-my.sharepoint.com/personal/dharley_essexmortgage_com/_layouts/15/embed.aspx?UniqueId=b1409f75-506a-4d88-8555-020680b224fb&embed=%7B%22ust%22%3Afalse%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create&showinfo=false"
-      width="800"
-      height="450"
-      allowFullScreen
-      className="rounded-xl pt-4 w-full max-w-4xl"
-      title="Essex DPA Webinar Recording"
-    />
-  </div>
-
-  <p className="text-gray-600 text-center text-lg pb-6">
-    2026 NATIONAL DPA PROGRAM WEBINAR
-  </p>
-</section>
+        {" "}
+        <div className="m-2 p-2 gap-4">
+          {" "}
+          <a
+            href="https://drive.google.com/file/d/1EnQW7Y8kMxsVz7OMZc0SISZ15G1Sk47G/view?usp=sharing"
+            target="_blank"
+          >
+            <Image
+              src={image1}
+              width={800}
+              height={100}
+              alt="Essex Mortgage DPA Webinar"
+              className="pt-4"
+            />{" "}
+          </a>{" "}
+        </div>{" "}
+        <p className="text-gray-600 text-center text-lg pb-6">
+          {" "}
+          2026 NATIONAL DPA PREOGRAM WEBINAR{" "}
+        </p>{" "}
+      </section>
     </>
   );
 };

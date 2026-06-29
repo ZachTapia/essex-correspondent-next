4e9b6457005a6c7c8e5e99288bdb7aea7ea56a83
Bradyn Wolkoys <bnwolkoys@gmail.com>
2026-06-29 14:10:28 -0700

    updated territory map

--- app/about/components/TerritoryMap.tsx
+++ app/about/components/TerritoryMap.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import image1 from "../../../public/about-page/TMAP.svg"
+import image1 from "../../../public/about-page/CorrTerritoryMap.png"
 
 const TerritoryMap = () => {
     return (
